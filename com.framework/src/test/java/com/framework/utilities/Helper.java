00b24bc2b051c2496451ca26e32bdfd770be1466
Sathish Kumar <ksathish@ad.shravas.com>
2019-09-06 18:09:38 +0530

    updated library files

--- com.framework/src/test/java/com/framework/utilities/Helper.java
+++ com.framework/src/test/java/com/framework/utilities/Helper.java
@@ -15,12 +15,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class Helper {
 	
 	private static String timestamp;
 	//private static String time;
-		
+	
 	public static String getCurrentDate()
 	{
 		DateFormat date = new SimpleDateFormat("MM_dd_yyyy_HH_mm_ss");
@@ -76,5 +78,98 @@ public class Helper {
 		jse.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid yellow;');",
 				Webelement);
 		jse.executeScript("arguments[0].setAttribute('style', '" + originalStyle + "');", Webelement);
+	}
+	
+	public static WebElement getElementByProperty(WebDriver driver, String property_value, int time) {
+		@SuppressWarnings("deprecation")
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(time, TimeUnit.SECONDS)
+				.pollingEvery(5, TimeUnit.SECONDS).ignoring(Throwable.class);
+		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath((property_value))));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath((property_value))));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath((property_value))));
+		WebElement element = driver.findElement(By.xpath((property_value)));
+		elementHighlight(driver, element, 8);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return element;
+	}
+	
+	public static void enterTextUsingJavaScriptExecutor(WebDriver driver, By locator, String text, String locatorType) {
+		WebDriverWait wait = new WebDriverWait(driver, 20);
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		if (locatorType.equals("xpath")) {
+			wait.until(ExpectedConditions.elementToBeClickable(locator));
+			wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+			WebElement ele = driver.findElement(locator);
+			js.executeScript("arguments[0].value='" + text + "'", ele);
+		}   
+	}
+	
+	public static WebElement getElementByXpath(WebDriver driver, String locator, int time) throws InterruptedException {
+		@SuppressWarnings("deprecation")
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(time, TimeUnit.SECONDS)
+				.pollingEvery(5, TimeUnit.SECONDS).ignoring(Throwable.class);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+		WebElement element = driver.findElement(By.xpath(locator));
+		elementHighlight(driver, element,8);
+		Thread.sleep(500);
+		return element;
+	}
+	
+	public static WebElement getElementByClassName(WebDriver driver, String locator, int time) throws InterruptedException {
+		@SuppressWarnings("deprecation")
+		FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver).withTimeout(time, TimeUnit.SECONDS)
+				.pollingEvery(5, TimeUnit.SECONDS).ignoring(Throwable.class);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(locator)));
+		wait.until(ExpectedConditions.elementToBeClickable(By.className(locator)));
+		WebElement element = driver.findElement(By.className(locator));
+		elementHighlight(driver, element,8);
+		Thread.sleep(500);
+		return element;
+	}
+	
+	public static void webDriverwait_keyvalue(WebDriver driver,String text) throws Exception {
+		
+		WebDriverWait wait = new WebDriverWait(driver, 100);
+		// explicit wait for property values
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath((text))));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath((text))));
+	}
+
+	public static void webDriverwait_locator(WebDriver driver,String locator, String locatorType) throws Exception {
+		
+		WebDriverWait wait = new WebDriverWait(driver, 100);
+		// explicit wait for locator values
+		if (locatorType.equalsIgnoreCase("xpath")) {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(locator)));
+			wait.until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
+		} else if (locatorType.equalsIgnoreCase("classname")) {
+			wait.until(ExpectedConditions.visibilityOfElementLocated(By.className(locator)));
+			wait.until(ExpectedConditions.elementToBeClickable(By.className(locator)));
+		}
+	}
+	
+	public static boolean DownloadFileVerify(WebDriver driver,String property) throws Exception {
+		// Verify the downloaded file as excel by comparing the filename title and
+		// downloaded file name
+		Thread.sleep(5000);
+		String series_title = driver.findElement(By.xpath((property))).getText();
+		String downloadPath = System.getProperty("user.dir");
+		File dir = new File(downloadPath);
+		File[] dirContents = dir.listFiles();
+		for (int i = 0; i < dirContents.length; i++) {
+			if (dirContents[i].getName().contains(series_title)) {
+				// File has been found, it can now be deleted:
+				dirContents[i].delete();
+				System.out.println("File has been download to Excel and its verified");
+				return true;
+			}
+		}
+		Assert.fail("Download to Excel verification failed");
+		return false;
 	}
 }
